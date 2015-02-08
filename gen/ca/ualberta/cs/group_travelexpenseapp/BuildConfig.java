0a5453fab4f0083e79c5591ef6662e6499454db8
yeni94 <omoyeni@ualberta.ca>
2015-02-08 15:27:38 -0700

    Main user interface

---
+++ gen/ca/ualberta/cs/group_travelexpenseapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ca.ualberta.cs.group_travelexpenseapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
